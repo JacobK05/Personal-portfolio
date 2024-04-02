8d0e0de6275d619d8107400968b371b958e5843c
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-02 14:05:52 -0500

    added the sections for the projects page

--- portfolio/src/pages/projects.jsx
+++ portfolio/src/pages/projects.jsx
@@ -1,7 +1,27 @@
-export default function project (){
-    return (
-        <div>
-            <p> hello </p>
-        </div>
-    )
+import project from "../projects";
+
+export default function PortfolioPage() {
+  return (
+    <div class="container">
+      <h3>Portfolio</h3>
+
+      <div class="row">
+        {project.map((props) => (
+          <div class="card col-md-6 col-lg-4 col-xl-3">
+            <p>
+              <a href={props.deployed}>
+                <img
+                  class="images"
+                  src={props.image}
+                  alt="Image goes here"
+                ></img>
+              </a>
+            </p>
+            <div class="text-block"><h4>{props.name}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ portfolio/src/projects.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+        name: 'Password Generator',
+        link: '',
+        image: 'portfolio/assets/password-generator.png',
+        deployed: ''
+    },
+    {
+        name: 'J.A.T.E',
+        link: 'https://github.com/JacobK05/pwa-challenge',
+        image: '',
+        deployed: 'https://pwa-challenge-4b3i.onrender.com/.'
+    },
+    {
+        name: 'Restaurant and hotel explorer',
+        link: 'https://github.com/JacobK05/project_1',
+        image: '',
+        deployed: 'https://jacobk05.github.io/project_1/'
+    },
+    {
+        name: 'Wedding Planner ...Under construction',
+        link: '',
+        image: '',
+        deployed: ''
+    },
+  
+];
+
+export default projects;
