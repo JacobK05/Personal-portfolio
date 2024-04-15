dfb7a37af5f4d3bf71ac33bc91aeae62437297e4
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-04-15 15:49:31 -0500

    added a link to resume

--- portfolio/src/pages/resume.jsx
+++ portfolio/src/pages/resume.jsx
@@ -1,7 +1,8 @@
+
 export default function resume (){
     return (
         <div>
-            <h1 className="resumeH1"> View My Resume <a href="" className="resume-link">here</a> </h1>
+            <h1 className="resumeH1"> View My Resume <a href="https://www.linkedin.com/in/jacob-kaufenberg-324a122a7/" className="resume-link">here</a> </h1>
          <section className="skill-list">
             <h1 className="frontH1"> Front end - Proficiencies </h1>
             <ul>
